8436e0424053b53c0e06f177b6f0b6bc60887385
Andrea Cruz <andrea@treinamento.com>
2021-08-10 19:10:21 -0300

    First commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,26 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+	
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		Livro livro = new Livro();
+		
+//		int a = 2;
+//		int b = 5;
+		
+//		System.out.println("Hello World! " + a + b);
+		System.out.println(gato);
+		
+	}
+}
+
+class Livro{
+	
+	private String nome;
+	private String nPag;
+	
+}
